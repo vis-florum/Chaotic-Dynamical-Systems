18b4a0d74fc942dcc42d45653044a5503d0703f8
Johannes Huber <tallskogsjakt@gmail.com>
2017-03-29 22:14:33 +0200

    Iteration Analysis fix

--- iterationAnalysis.m
+++ iterationAnalysis.m
@@ -1,18 +1,17 @@
+% Analyse pathes of a seeds during iteration of a function
+
 clear all
 clc
-
-% Analyse the path of a seed during iteration of a function
-
 
 %% Plot the development of the orbit for a function:
 clear all
 clc
 % Intressant: Q_-2 mit seed 0.3
 N = 4;
-fifi = @(x) x.^2 -.1;
+chaosfct = @(x) x.^2 -.1;
 seed = 0.8;
 
-[x,y] = orbitCoords(seed,N,fifi);
+[x,y] = orbitCoords(seed,N,chaosfct);
 
 plotmax = max(abs([x,y]))*1.1;
 plotmin = -plotmax;
@@ -25,7 +24,7 @@ for i = 1:1:length(x)
     hold on
     title(['iter = ',num2str(floor(i/2))])
     % Function:
-    fplot(fifi,[plotmin plotmax]*2)
+    fplot(chaosfct,[plotmin plotmax]*2)
     % Diagonal:
     fplot(@(x) x,[plotmin plotmax]*2,'Linewidth',2,'--')
     % Vertical:
@@ -45,25 +44,25 @@ for i = 1:1:length(x)
     pause(.2)
 end
 
-%% Plot orbits for different seeds sequentially for a function:
+%% Plot orbits for different seeds sequentially for a function
 clear all
 clc
 N = 20;
-fifi = @(x) x.^2 -.1;
+chaosfct = @(x) x.^2 -.1;
 
 plotmax = 1.5;
 plotmin = -plotmax;
 
 for seed = -1:.01:1
     
-    [x,y] = orbitCoords(seed,N,fifi);
+    [x,y] = orbitCoords(seed,N,chaosfct);
     
     % Orbit:
     plot(x,y)
     hold on
     title(['seed = ',num2str(seed)])
     % Function:
-    fplot(fifi,[plotmin plotmax]*2)
+    fplot(chaosfct,[plotmin plotmax]*2)
     % Diagonal:
     fplot(@(x) x,[plotmin plotmax]*2,'Linewidth',2,'--')
     % Vertical:
@@ -83,28 +82,28 @@ for seed = -1:.01:1
     pause(.02)
 end
 
-
-%% Plot orbits for different parameter sequentially
+%% Plot orbits for different parameters sequentially
 clear all
 clc
 N = 20;
+
 seed = 0.1;
 
 plotmax = 2;
 plotmin = -plotmax;
 
-for c = 2:-.01:-2
+for c = 2:-.02:-2
     
-    %fifi = @(x) x.^2 + c;
+    chaosfct = @(x) x.^2 + c;
     %fifi = @(x) x.^-c - 1;
-    [x,y] = orbitCoords(seed,N,fifi);
+    [x,y] = orbitCoords(seed,N,chaosfct);
     
     % Orbit:
     plot(x,y)
     hold on
     title(['c = ',num2str(c)])
     % Function:
-    fplot(fifi,[plotmin plotmax]*2)
+    fplot(chaosfct,[plotmin plotmax]*2)
     % Diagonal:
     fplot(@(x) x,[plotmin plotmax]*2,'Linewidth',2,'--')
     % Vertical:
